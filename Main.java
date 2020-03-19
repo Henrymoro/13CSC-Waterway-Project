dd5bb9dd210e2f2e0e594dda1eca8f731f86deaf
Molly <henrymo@whs.school.nz>
2020-03-20 12:52:01 +1300

    Created an initial menu example.

--- Main.java
+++ Main.java
@@ -12,17 +12,15 @@ import java.util.Scanner;
 
 public class Main extends JFrame
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem menuItem;
     
     /**
      * Constructor for objects of class Main
      */
     public Main()
     {
-        // initialise instance variables
-        x = 0;
-        
         Scanner inputStream = new Scanner(System.in);
         System.out.println("The window is called:");
         String windowName = inputStream.nextLine();
@@ -34,22 +32,33 @@ public class Main extends JFrame
         
         this.getContentPane().setPreferredSize(new Dimension(dimensionsX,dimensionsY));
         
+        menuBar=new JMenuBar();
+        this.setJMenuBar(menuBar);
+        
+        menu = new JMenu("Numbers");
+        menuBar.add(menu);
+        
+        menuItem=new JMenuItem("The First");
+        menu.add(menuItem);
+        menuItem=new JMenuItem("The Second");
+        menu.add(menuItem);
+        menuItem=new JMenuItem("The Third");
+        menu.add(menuItem);
+        
+        menu = new JMenu("Words");
+        menuBar.add(menu);
+        
+        menuItem=new JMenuItem("The Apple");
+        menu.add(menuItem);
+        menuItem=new JMenuItem("The Banana");
+        menu.add(menuItem);
+        menuItem=new JMenuItem("The Carrot");
+        menu.add(menuItem);
+        
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         
         this.pack();
         this.toFront();
         this.setVisible(true);
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
     }
 }
